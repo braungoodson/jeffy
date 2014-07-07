410a4a7929146ba7e83cd7eacb130aeda72f0df1
braun <braun@infinity.local>
2014-07-07 19:29:26 -0400

    adding modules.html

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app
 	.config(['$routeProvider',function($routeProvider){
 		$routeProvider.when('/',{templateUrl:'views/index.html'});
 		$routeProvider.when('/getting-started',{templateUrl:'views/getting-started.html'});
+		$routeProvider.when('/modules',{templateUrl:'views/modules.html'});
 		$routeProvider.otherwise({redirectTo:'/'});
 	}])
 	.directive('jeffyNavigation',['$location',function($location){
